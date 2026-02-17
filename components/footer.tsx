734ca88e0637055044c2273c45897f687e5e01d6
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-02-17 19:34:47 +0600

    footer dynamic

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { useLanguage } from "@/lib/context/LanguageContext";
+"use client";
 import { Heart } from "lucide-react";
+import { useLanguageStore } from "@/zustand/useLanguageStore";
+import { useQuery } from "@tanstack/react-query";
 
 const Footer = () => {
-  const { t } = useLanguage();
+  const { lang } = useLanguageStore();
+
+  const { data: footerData } = useQuery({
+    queryKey: ["footer", lang],
+    queryFn: async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/footer?lang=${lang}`,
+      );
+      const data = await res.json();
+      return data?.data;
+    },
+  });
+
+  if (!footerData) {
+    return null; // or a loading skeleton
+  }
+
+  const { partnerOne, partnerTwo, date, footerNote } = footerData;
 
   return (
     <footer className="bg-primary text-white py-16">
@@ -16,29 +34,19 @@ const Footer = () => {
 
           {/* Names */}
           <h3 className="font-serif text-2xl md:text-3xl font-medium tracking-wide">
-            Axel-Philippe & Eunice
+            {partnerOne} & {partnerTwo}
           </h3>
 
           {/* Date */}
           <p className="text-xs uppercase tracking-[0.3em] opacity-80">
-            {t.hero.date}
+            {date}
           </p>
 
           {/* Divider */}
           <div className="w-16 h-px bg-white/30 my-6" />
 
           {/* Credit */}
-          <p className="text-[11px] ">
-            Made with love by{" "}
-            <a
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="underline underline-offset-2 hover:opacity-100 transition"
-            >
-              The Digital Yes
-            </a>
-          </p>
+          <p className="text-[11px]">{footerNote || "Made with love by"} </p>
         </div>
       </div>
     </footer>
